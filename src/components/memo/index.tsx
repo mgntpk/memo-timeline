4722887d3aa6d30aed623af58caf2d8cfae1bde7
mgntpk <mgntpkgit@gmail.com>
2022-10-14 23:28:42 +0900

    feat:memoコンポーネントを追加。

---
+++ src/components/memo/index.tsx
@@ -0,0 +1,14 @@
+import styles from './.module.css'
+
+const memo = (props : { content: string ; date: string ; key : number }) =>  {
+    return (
+        <div className={styles.container}>
+            <div className={styles.memoContainer}>
+                <div className={styles.content}>{props.content}</div>
+                <div className={styles.date}>{props.date}</div>
+            </div>
+        </div>
+    )
+}
+
+export default memo
