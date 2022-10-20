47c0b3d678273a0f0ec6364f5636b766a755cb30
mgntpk <mgntpkgit@gmail.com>
2022-10-20 22:27:00 +0900

    feat:memoTimelineコンポーネントを追加。

---
+++ src/components/memoTimeline/index.tsx
@@ -0,0 +1,14 @@
+import styles from './.module.css'
+import Memo from "../memo";
+const memoTimeline = (props: { memos: { id: number; content: string; date: string; }[] }) => {
+    const memoList = props.memos.map((data: { id: number; content: string; date: string; }) => (
+        <Memo
+            key = {data.id}
+            content = {data.content}
+            date = {data.date}
+        />
+    ));
+    return <div className = "timeline">{memoList}</div>
+}
+
+export default memoTimeline
