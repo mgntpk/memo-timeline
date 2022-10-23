24b8a995c73bcfdfae48cc5d27f05375cc46d401
mgntpk <mgntpkgit@gmail.com>
2022-10-23 22:45:07 +0900

    refactor:型定義を追加・修正。

--- src/components/memoTimeline/index.tsx
+++ src/components/memoTimeline/index.tsx
@@ -1,6 +1,6 @@
 import styles from './.module.css'
 import Memo from "../memo";
-const memoTimeline = (props: { memos: { id: number; content: string; date: string; }[] }) => {
+const memoTimeline = (props: { memos: Array<{ id: number; content: string; date: string; }> }) => {
     const memoList = props.memos.map((data: { id: number; content: string; date: string; }) => (
         <Memo
             key = {data.id}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,11 @@ import MemoTimeline from "../components/memoTimeline"
 import MemoInput from "../components/memoInput"
 import { useCallback, useEffect, useState } from 'react'
 const Home: NextPage = () => {
-	const [memos,setMemos] = useState<any[]>([]);
+	const [memos,setMemos] = useState<Array<{ id: number; content: string; date: string; }>>([]);
 	useEffect(() => {
 		if(localStorage.memo){
-			const localStorageMemo : { id: number; content: string; date: string; }= JSON.parse(localStorage.memo);
-			setMemos([localStorageMemo]);
+			const localStorageMemo : Array<{ id: number; content: string; date: string; }>= [JSON.parse(localStorage.memo)];
+			setMemos(localStorageMemo);
 		}
 	}, []);
 	const addMemo = useCallback((object:{ id: number; content: string; date: string; }) =>setMemos((property) => [object,...property]),[setMemos]);
