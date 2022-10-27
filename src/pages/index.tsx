abf53b479c85ab053cd1b566ba9440a7490cf45e
mgntpk <mgntpkgit@gmail.com>
2022-10-27 21:17:00 +0900

    refactor:不要な宣言を省略。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,7 @@ import { useCallback, useEffect, useState } from 'react'
 const Home: NextPage = () => {
 	const [memoArray,setMemoArray] = useState<Array<{ id: number; content: string; date: string; }>>(([]));
 	useEffect(() => {
-		if(localStorage.memo){
-			const localStorageMemo : Array<{ id: number; content: string; date: string; }>= JSON.parse(localStorage.memo);
-			setMemoArray(localStorageMemo);
-		}
+		if(localStorage.memo)setMemoArray(JSON.parse(localStorage.memo));
 	}, []);
 	const addMemo = useCallback((object:{ id: number; content: string; date: string; }) =>{
 		const memoArray_ = [object,...memoArray];
